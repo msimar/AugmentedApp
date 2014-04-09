6e3dd9b353fd88476aa3db63cfd29151fd1bb9f9
ManinderPal Singh <manni.cse@gmail.com>
2014-04-09 23:53:26 +0300

    Changed url for a audio downloads.

--- src/com/univ/helsinki/app/util/Constant.java
+++ src/com/univ/helsinki/app/util/Constant.java
@@ -5,6 +5,7 @@ public class Constant {
 	
 	// http://www.virginmegastore.me/Library/Music/CD_001214/Tracks/Track1.mp3
 	public static final String[] AUDIO_URL = new String[]{
-			"http://www.youtube-mp3.org/get?ab=128&video_id=IitbJszd1kM&h=7b51e7f4a9c388a4d4ba42bd3bb50061&r=1397038109462.1609762482"
+		"http://www.virginmegastore.me/Library/Music/CD_001214/Tracks/Track1.mp3"
+			//"http://www.youtube-mp3.org/get?ab=128&video_id=IitbJszd1kM&h=7b51e7f4a9c388a4d4ba42bd3bb50061&r=1397038109462.1609762482"
 	};
 }
